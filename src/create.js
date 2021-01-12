8fb90441d7e73a56ef5f2759f4cad9db3d2f9f11
nestor <nestor.edrev@gmail.com>
2021-01-12 12:29:01 +0100

    example of and range

--- src/create.js
+++ src/create.js
@@ -1,9 +1,24 @@
 import { displayLog } from './utils';
-import { of, interval, zip } from 'rxjs';
+import { of, range } from 'rxjs';
+
+/**
+ * of - crear secuencia de observables practicamente de cualquier tipo de dato.
+ */
 
 export default () => {
-    /** start coding */
+    
+    const source = of(1,2,3,4,5,6);
+    const source2 = of(
+        [10, 11, 12],
+        `Nestor Edrev`,
+        {apellidos: 'Dobrinov'},
+        () => {
+            return `Hola amigo`
+        }
+    )
 
+    // range - secuencia ordenada de numeros
+    const source3 = range(3, 10);
 
-    /** end coding */
+    const subscription = source3.subscribe(data => displayLog(data));
 }
