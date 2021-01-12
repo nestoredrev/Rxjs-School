571a03404b009c914188543ff3ac713bea65172f
nestor <nestor.edrev@gmail.com>
2021-01-12 15:39:40 +0100

    example mapTo, map and filter

--- src/create.js
+++ src/create.js
@@ -1,9 +1,28 @@
 import { displayLog } from './utils';
-import { of, interval, zip } from 'rxjs';
+import { from, fromEvent } from 'rxjs';
+import { mapTo, map, filter } from 'rxjs/operators';
+
+/**
+ * mapTo - por cada evento siempre devuelve el mismo valor (valor constante)
+ */
 
 export default () => {
-    /** start coding */
 
+    const grid = document.getElementById('grid');
+    const clickSource = fromEvent(grid, 'click').pipe(
+        map(val => {
+            // Devuelve la posicion de la casilla del grid
+            return [
+                Math.floor(val.offsetX / 50), 
+                Math.floor(val.offsetY / 50)
+            ]
+        }),
+        filter( val => { 
+            //Segun los valroes que devuelva el map
+            // solo se mostraran las casillas impares. 
+            return (val[0] + val[1]) % 2 != 0;
+        }) 
+    )
+    const subscription = clickSource.subscribe(data => displayLog(data));
 
-    /** end coding */
 }
