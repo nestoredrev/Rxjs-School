e786d006acddfda9f2fc2050e0fb7387f64a9d7a
nestor <nestor.edrev@gmail.com>
2021-01-12 12:16:08 +0100

    example from

--- src/create.js
+++ src/create.js
@@ -1,9 +1,27 @@
 import { displayLog } from './utils';
 import { of, interval, zip } from 'rxjs';
+import { from } from 'rxjs';
+
+/**
+ * from sirve para crear observables a partir de arrays o promesas. 
+ * Por cada elemento de array es un evento.
+ * Tambien sirve para convertir a una promesa a observable.
+ */
 
 export default () => {
-    /** start coding */
 
+    const myArray = [1,2,3,4,5];
+    const myString = `Nestor Edrev`;
+    const myPromise = new Promise( (resolve, reject) => {
 
-    /** end coding */
+        setTimeout(() => {
+            resolve(`My promise its DONE`);
+        }, 2000)
+    })
+
+    //const observable = from(myArray);
+    //const observable = from(myString);
+    const observable = from(myPromise);
+    observable.subscribe( event => displayLog(event));
+
 }
