d65ecb3a297987c31116d60dbe7454e1b4fc4b53
nestor <nestor.edrev@gmail.com>
2021-01-12 13:05:04 +0100

    example fromEvent

--- src/create.js
+++ src/create.js
@@ -1,9 +1,21 @@
 import { displayLog } from './utils';
-import { of, interval, zip } from 'rxjs';
+import { fromEvent } from 'rxjs';
+
+/**
+ * fromEvent - sobreescribre sobre cualquier evento de la interfaz (dom).
+ */
 
 export default () => {
-    /** start coding */
+
+    const myBtn = document.getElementById('myButton');
+    const source = fromEvent(myBtn, 'click');
+    source.subscribe(evento => {
+        displayLog(`Click event ap pos ${evento.x} - ${evento.y}`);
+    })
 
 
-    /** end coding */
+    fromEvent(document, 'mousemove').subscribe(pos => {
+        console.log(pos);
+    })
+
 }
